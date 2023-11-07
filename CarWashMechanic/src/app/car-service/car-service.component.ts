9785f2a699ae1684273e3f43597de9f7b2ba42ac
UmeshMareboina <mareboinaumesh07@gmail.com>
2023-11-07 19:22:40 +0530

    mat cards services list

--- CarWashMechanic/src/app/car-service/car-service.component.ts
+++ CarWashMechanic/src/app/car-service/car-service.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RegisterService } from '../services/register.service';
 
 @Component({
   selector: 'app-car-service',
   templateUrl: './car-service.component.html',
   styleUrls: ['./car-service.component.css']
 })
-export class CarServiceComponent {
+export class CarServiceComponent implements OnInit {
 
+  public ServiceList :any;
+
+constructor(private regServ : RegisterService, private router:Router){
+}
+
+ngOnInit(): void{
+  this.ServiceList = this.regServ.getServices().subscribe(result=>{
+    this.ServiceList = result;
+    console.log("Result",this.ServiceList)
+  });
+}
+addCart(){
+  this.router.navigate(['/login'])
+}
 }
